3530e52c513dd1f7c284536faf35ad122adc472f
Randy Butts <randybuttsii@gmail.com>
2022-12-11 08:23:28 -0500

    Replaced dms to decimal with builtin utility more unit tests

--- photos/tasks.py
+++ photos/tasks.py
@@ -3,23 +3,10 @@ from typing import Optional
 import blurhash, exifread
 from huey.contrib.djhuey import db_task, task
 from django.utils import timezone as tz
+from exifread.utils import get_gps_coords
 
 
 logger = logging.getLogger(__name__)
-
-
-# stolen from https://gist.github.com/snakeye/fdc372dbf11370fe29eb
-def _convert_to_degress(value) -> float:
-    """Helper function to convert the GPS coordinates stored in the EXIF to degress in float format
-
-    Args:
-        value: The coordinate in dms format
-    """
-    d = float(value.values[0].num) / float(value.values[0].den)
-    m = float(value.values[1].num) / float(value.values[1].den)
-    s = float(value.values[2].num) / float(value.values[2].den)
-
-    return d + (m / 60.0) + (s / 3600.0)
 
 
 def _parse_tag(tag: str, tags: dict) -> Optional[str]:
@@ -44,17 +31,10 @@ def extract_exif(photo):
     tags = exifread.process_file(photo.image)
 
     if "GPS GPSLatitude" in tags and "GPS GPSLongitude" in tags:
-        lat = _convert_to_degress(tags["GPS GPSLatitude"])
-        lon = _convert_to_degress(tags["GPS GPSLongitude"])
+        coords = get_gps_coords(tags)
 
-        if tags["GPS GPSLatitudeRef"].values != "N":
-            lat = 0 - lat
-
-        if tags["GPS GPSLongitudeRef"].values != "E":
-            lon = 0 - lon
-
-        photo.gps_lat = lat
-        photo.gps_lon = lon
+        photo.gps_lat = coords[0]
+        photo.gps_lon = coords[1]
 
     if "EXIF DateTimeOriginal" in tags and tags["EXIF DateTimeOriginal"].values.strip():
         try:
--- photos/tests/test_tasks.py
+++ photos/tests/test_tasks.py
@@ -1,30 +1,76 @@
+from decimal import Decimal
 from io import BytesIO
 from PIL import Image
 from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
+import exifread
+from exifread.utils import Ratio
+from unittest.mock import Mock
+from django.utils import timezone as tz
 
 from photos.models import Photo
+from photos.tasks import (
+    _parse_tag,
+    extract_exif,
+    generate_blurhash,
+    process_photo,
+)
+
+
+class TaskHelpersTestCase(TestCase):
+    def test__parse_tag__present(self):
+        m = Mock()
+        m.values = "Test Value"
+
+        tags = {
+            "Test Tag": m,
+        }
+
+        self.assertEqual(
+            _parse_tag(
+                "Test Tag",
+                tags,
+            ),
+            "Test Value",
+        )
+
+    def test__parse_tag__missing(self):
+        m = Mock()
+        m.values = "Test Value"
+
+        tags = {
+            "Test Tag": m,
+        }
+
+        self.assertIsNone(
+            _parse_tag(
+                "Test Fake Tag",
+                tags,
+            )
+        )
 
 
 class PhotoTasksTestCase(TestCase):
-    def setUp(self):
-        testImg = Image.new("RGB", (1000, 500))
-        testImgIO = BytesIO()
-        testImg.save(testImgIO, "JPEG")
-        testImgIO.seek(0)
+    def test_extract_exif(self):
+        with open("photos/fixtures/EXIF_Test.jpg", "rb") as f:
+            p = Photo.objects.create(
+                title="Test Photo",
+                image=SimpleUploadedFile(
+                    "test.jpg",
+                    content=f.read(),
+                    content_type="image/jpeg",
+                ),
+            )
 
-        self.photo = Photo.objects.create(
-            title="Test Photo",
-            image=SimpleUploadedFile(
-                name="test.jpg", content=testImgIO.read(), content_type="image/jpeg"
-            ),
+        extract_exif(p)
+        p.refresh_from_db()
+
+        self.assertEqual(p.gps_lat, Decimal("43.467157"))
+        self.assertEqual(p.gps_lon, Decimal("11.885395"))
+        self.assertEqual(
+            p.date_taken, tz.make_aware(tz.datetime(2008, 10, 22, 16, 29, 49))
         )
-
-    def test__str__(self):
-        self.assertEqual(str(self.photo), "Test Photo")
-
-    def test_aspect_ratio(self):
-        self.assertEqual(self.photo.aspect_ratio, 2.0)
-
-    def test_image_url(self):
-        self.assertEqual(self.photo.image_url, "/photo/serve/1/")
+        self.assertEqual(p.camera_make, "NIKON")
+        self.assertEqual(p.camera_model, "COOLPIX P6000")
+        self.assertEqual(p.lens_make, "TEST LENS MAKE")
+        self.assertEqual(p.lens_model, "TEST LENS MODEL")
