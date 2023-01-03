72bd409a45db0f85b1936e54c456a51feeedc9ad
Randy Butts <randybuttsii@gmail.com>
2023-01-03 03:13:41 -0500

    Added comments to photo detail

--- webui/src/pages/PhotoDetail.vue
+++ webui/src/pages/PhotoDetail.vue
@@ -3,16 +3,20 @@
         <div class="mt-n2 flex-grow-1 d-flex flex-column justify-content-between position-relative">
             <div class="ps-3 pt-3 bg-light w-100 border-bottom border d-flex z-2">
                 <div class="flex-grow-1">
+                    <!-- Show edit on click, otherwise show the breadcrumb -->
                     <div v-show="editing">
+                        <!-- Title Update Widget -->
                         <input ref="titleInput" type="text" class="form-control" v-model="photoTitle"
                             @change="updateTitle" @keyup.enter="updateTitle">
                     </div>
                     <div v-if="!editing" @click="editTitle">
-                        <h3 class="">
+                        <h3>
                             <Breadcrumbs :crumbs="crumbs"></Breadcrumbs>
                         </h3>
                     </div>
                 </div>
+
+                <!-- Sidebar Collapse Button -->
                 <button class="btn btn-outline-info me-3 mb-3" @click="sidebarCollapsed = !sidebarCollapsed"
                     type="button" data-bs-toggle="collapse" data-bs-target="#photoSidebar" aria-expanded="false"
                     aria-controls="photoSidebar">
@@ -20,25 +24,32 @@
                     <span v-else><i class="bi bi-info-circle-fill"></i></span>
                 </button>
             </div>
+            <!-- Image holder, captures hooks for pz mouse events -->
             <div class="flex-grow-1 overflow-hidden d-flex justify-content-center w-100 position-relative"
                 @wheel.prevent="scaleImage" @mousemove.prevent="offsetImage" @mousedown="() => panningImage = true"
                 @mouseup="() => panningImage = false">
                 <div class="position-absolute top-0 bottom-0 start-0 end-0">
+                    <!-- Image -->
                     <img ref="imageElement" class="image align-self-center" :src="photo.imageUrl" :style="{
                         transform: `scale(${imageScale}) translate(${imageOffsetX}px, ${imageOffsetY}px)`,
                     }" />
                 </div>
                 <div class="position-absolute top-0 bottom-0 end-0">
+                    <!-- Sidebar -->
                     <div id="photoSidebar" class="photo-sidebar bg-light border-start border-bottom collapse p-3 z-2">
+                        <!-- Creator -->
                         <div class="mb-3" v-if="photo.creator">
                             <span class="fw-bold d-block">Creator</span>
                             <span class="">{{ photo.creator.username }}</span>
                         </div>
+
+                        <!-- Date Taken -->
                         <div class="mb-3" v-if="photo.dateTaken">
                             <span class="fw-bold d-block">Date Taken</span>
                             <span class="">{{ new Date(photo.dateTaken).toLocaleString() }}</span>
                         </div>
 
+                        <!-- GPS Location -->
                         <div class="mb-3" v-if="photo.gpsLat && photo.gpsLon">
                             <span class="fw-bold d-block">Location</span>
                             <span class="">
@@ -49,6 +60,7 @@
                             </span>
                         </div>
 
+                        <!-- Camera make model -->
                         <div class="mb-3" v-if="photo.cameraMake">
                             <span class="fw-bold d-block">Camera Make</span>
                             <span class="">{{ photo.cameraMake }}</span>
@@ -58,6 +70,7 @@
                             <span class="">{{ photo.cameraModel }}</span>
                         </div>
 
+                        <!-- Lens make model -->
                         <div class="mb-3" v-if="photo.lensMake">
                             <span class="fw-bold d-block">Lens Make</span>
                             <span class="">{{ photo.lensMake }}</span>
@@ -70,11 +83,15 @@
                     </div>
                 </div>
             </div>
+
+            <!-- Previous Image Button -->
             <div class="position-absolute top-0 bottom-0 start-0 d-flex justify-content-center z-1 bg-white"
                 style="--bs-bg-opacity: .2;">
                 <button class="btn" @click="$router.push(previousPage as RouteLocationRaw)"
                     :disabled="previousPage == null"><i class="bi bi-chevron-left"></i></button>
             </div>
+
+            <!-- Next Image Button -->
             <div class="position-absolute top-0 bottom-0 end-0 d-flex justify-content-center z-1 bg-white"
                 style="--bs-bg-opacity: .2;">
                 <button class="btn" @click="$router.push(nextPage as RouteLocationRaw)" :disabled="nextPage == null"><i
@@ -84,6 +101,7 @@
         </div>
 
         <div>
+            <!-- Debug Info -->
             <button class="btn btn-secondary" type="button" data-bs-toggle="collapse" data-bs-target="#debugInfo"
                 aria-expanded="false" aria-controls="debugInfo">
                 Show Debug Info
@@ -145,6 +163,8 @@ const props = defineProps<{
 }>();
 
 const slideList = computed(() => {
+    // Fetch the slide list from the history state, slide list is the photo ids in the collection this photo was in
+
     if (!history.state.slideList) {
         return null;
     }
@@ -152,6 +172,8 @@ const slideList = computed(() => {
 })
 
 const previousPage = computed(() => {
+    // Get the previous photo url in the slide list
+
     if (!slideList.value) {
         return null;
     }
@@ -169,6 +191,8 @@ const previousPage = computed(() => {
 })
 
 const nextPage = computed(() => {
+    // Get the next photo url in the slide list
+
     if (!slideList.value) {
         return null;
     }
@@ -187,6 +211,8 @@ const nextPage = computed(() => {
 
 
 function handleNavKeys(e: KeyboardEvent) {
+    // Handle keydown events for navigating between photos
+
     if (e.key === 'ArrowLeft') {
         $router.push(previousPage.value as RouteLocationRaw);
     } else if (e.key === 'ArrowRight') {
@@ -195,14 +221,17 @@ function handleNavKeys(e: KeyboardEvent) {
 }
 
 onMounted(() => {
+    // Add keydown event listener for navigating between photos
     window.addEventListener('keydown', handleNavKeys)
 })
 
 onUnmounted(() => {
+    // Remove keydown event listener for navigating between photos
     window.removeEventListener('keydown', handleNavKeys)
 })
 
 onBeforeRouteUpdate((to, from) => {
+    // Reset image zoom and offset when navigating to a new photo
     if (to.params.id !== from.params.id) {
         editing.value = false;
         imageScale.value = 1;
@@ -213,6 +242,7 @@ onBeforeRouteUpdate((to, from) => {
 })
 
 
+// Query for the photo
 const { result: photoQuery } = useQuery(graphql(`
     query photo($id: ID!) {
         photo(pk: $id) {
@@ -240,6 +270,7 @@ const { result: photoQuery } = useQuery(graphql(`
     id: `${$route.params.id}`
 }))
 
+// Update the photo and title when the query result changes
 watch(photoQuery, (result) => {
     if (result == null) {
         return;
@@ -250,6 +281,7 @@ watch(photoQuery, (result) => {
 
 
 function editTitle() {
+    // Start editing the title
     if (titleInput.value == null) {
         return;
     }
@@ -261,6 +293,7 @@ function editTitle() {
 }
 
 
+// Mutation for updating the photo title
 const { mutate: updateTitleMutation } = useMutation(
     graphql(`
         mutation updatePhoto($id: ID!, $title: String!) {
@@ -273,6 +306,7 @@ const { mutate: updateTitleMutation } = useMutation(
 )
 
 function updateTitle() {
+    // Update the photo title using the updateTitleMutation
     updateTitleMutation({
         id: $route.params.id as string,
         title: photoTitle.value
@@ -286,6 +320,7 @@ function updateTitle() {
 }
 
 function scaleImage(event: WheelEvent) {
+    // Scale the image using the mouse wheel
     if (imageElement.value == null) {
         return;
     }
@@ -302,11 +337,14 @@ function scaleImage(event: WheelEvent) {
     }
 
     imageScale.value = newScale
+
+    // Adjust image offset so that the mouse position stays in the same place, could be improved
     imageOffsetX.value = (imageOffsetX.value * newScale) / scale
     imageOffsetY.value = (imageOffsetY.value * newScale) / scale
 }
 
 function offsetImage(event: MouseEvent) {
+    // Offset the image when the mouse is down
     if (!panningImage.value || imageElement.value == null) {
         return;
     }
@@ -316,6 +354,7 @@ function offsetImage(event: MouseEvent) {
 }
 
 const crumbs = computed(() => {
+    // Generate breadcrumbs for the photo detail page
     let crumbs = []
     if ($route.name == 'AlbumPhotoDetail') {
         crumbs.push({ name: 'AlbumList', label: 'Albums' })
